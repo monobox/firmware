fa3a660addcee58f3b6712e786e3d00b5b12e7e8
OXullo Intersecans <x@brainrapers.org>
2018-06-24 22:19:43 +0200

    Add a DAC / sample player tester

---
+++ experiments/dac/Inc/sampleplayer.h
@@ -0,0 +1,14 @@
+/*
+ * sampleplayer.h
+ *
+ *  Created on: Jun 24, 2018
+ *      Author: xi
+ */
+
+#ifndef SAMPLEPLAYER_H_
+#define SAMPLEPLAYER_H_
+
+void sampleplayer_init();
+void sampleplayer_play_click();
+
+#endif /* SAMPLEPLAYER_H_ */
